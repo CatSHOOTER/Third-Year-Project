36563222a9765bc9a58847233cf8de7790be502a
Daniel Reilly <S00157339@mail.itsligo.ie>
2018-02-13 18:49:15 +0000

    safety

--- Field of View/Assets/Scripts/SwitchWeapon.cs
+++ Field of View/Assets/Scripts/SwitchWeapon.cs
@@ -5,18 +5,13 @@ using UnityEngine.UI;
 public class SwitchWeapon : MonoBehaviour {
     public static int CurrentWeapon = 0;
     public  int CurWeapon = 0;
-<<<<<<< HEAD
-    public Text CWeapon;
-    public Text PWeapon;
-    public Text FWeapon;
-=======
     public RawImage CWeapon;
     public RawImage PWeapon;
     public RawImage FWeapon;
     public Text AmmoDisplay;
 
     public Texture sticky, bouncy, unarmed, Paint;
->>>>>>> 8e6c19a0f46f38ff56de1aaafe5b7f55a5811433
+
     // Use this for initialization
     void Start () {
 		
@@ -62,45 +57,24 @@ public class SwitchWeapon : MonoBehaviour {
         
         if(CurrentWeapon == 0)
         {
-<<<<<<< HEAD
-            CWeapon.text = "S";
-            PWeapon.text = "U";
-            FWeapon.text = "B";
-=======
             CWeapon.texture = sticky;
             PWeapon.texture = unarmed;
             FWeapon.texture = bouncy;
 
             AmmoDisplay.text = shooting.stickyAmmo.ToString();
->>>>>>> 8e6c19a0f46f38ff56de1aaafe5b7f55a5811433
+
         }
         else if(CurrentWeapon == 1)
         {
-<<<<<<< HEAD
-            CWeapon.text = "B";
-            PWeapon.text = "S";
-            FWeapon.text = "P";
-=======
             CWeapon.texture = bouncy;
             PWeapon.texture = sticky;
             FWeapon.texture = Paint;
 
             AmmoDisplay.text = shooting.bouncyAmmo.ToString();
->>>>>>> 8e6c19a0f46f38ff56de1aaafe5b7f55a5811433
+
         }
         else if(CurrentWeapon == 2)
         {
-<<<<<<< HEAD
-            CWeapon.text = "P";
-            PWeapon.text = "B";
-            FWeapon.text = "U";
-        }
-        else
-        {
-            CWeapon.text = "U";
-            PWeapon.text = "P";
-            FWeapon.text = "S";
-=======
             CWeapon.texture = Paint;
             PWeapon.texture = bouncy;
             FWeapon.texture = unarmed;
@@ -114,7 +88,7 @@ public class SwitchWeapon : MonoBehaviour {
             FWeapon.texture = sticky;
 
             AmmoDisplay.text = "~";
->>>>>>> 8e6c19a0f46f38ff56de1aaafe5b7f55a5811433
+
         }
     }
 }
